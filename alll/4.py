4a3f5baba751b1a125c355cbb81f713ec2b729e5
Wolgerrom5 <sofyaviktorovna26@gmail.com>
2024-04-20 08:15:38 +0700

    praktika_9

--- alll/4.py
+++ alll/4.py
@@ -4,7 +4,7 @@ while True:
     k = int(input('Введите количество веревок(или 0 для окончания): '))
     if k == 0:
         break
-    if k % 2 == 0 and k >= 6:
+    if k % 4 == 0:
         count += 1
 
 print(count)
